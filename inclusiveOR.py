2ac44ac2a636b5be0568412adbf95f348caa8f23
Joachim Vanneste <2460800V@student.gla.ac.uk>
2022-06-28 12:10:33 +0100

    learning phase added

--- inclusiveOR.py
+++ inclusiveOR.py
@@ -4,6 +4,7 @@ learing_rate = 1
 bias = 1
 weights = [random.random(), random.random(), random.random()]
 
+
 def Perceptron(input1, input2, output):
     perceptron_output = input1*weights[0]+input2*weights[1]+bias*weights[2]
     if perceptron_output > 0 : #activation function
@@ -23,3 +24,17 @@ for i in range(50):
     Perceptron(1, 0, 1)
     Perceptron(0, 1, 1)
     Perceptron(0, 0, 0)
+
+x = int(input("x = "))
+y = int(input("y = "))
+
+perceptron_output = x*weights[0]+y*weights[1]+bias*weights[2]
+
+if perceptron_output > 0 : #activation function
+   perceptron_output = 1
+else :
+   perceptron_output = 0
+print(x, "or", y, "is:", perceptron_output)
+
+
+#currently using heaviside activation function rather than sigmoid since we are looking for a true/false result
